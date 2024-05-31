8082a7b9ac1ac964e8e0ee392277d6ddf31f8416
Rivera <billy100007@gmail.com>
2024-05-31 12:49:07 +0800

    WORKING DATABASE CONNECTION

--- includes/formhandler.inc.php
+++ includes/formhandler.inc.php
@@ -1,5 +1,14 @@
 <?php
 
+$frametitle = $_POST["frametitle"];
+$description = $_POST["description"];
+$fileid = $_POST["fileid"];
+
 require_once "dbh.inc.php";
 
-$query = "INSERT INTO frame (frametitle,description,)";
+$query = "INSERT INTO frame (frametitle,description,fileid) VALUES (?,?,?);";
+$stmt = $pdo->prepare($query);
+
+
+$stmt->execute([$frametitle,$description,$fileid]);
+
