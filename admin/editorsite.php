4907559473bdf9783074fd3d0972768d150fe2b9
Rivera <billy100007@gmail.com>
2024-06-02 04:08:48 +0800

    dynamically added new  frames in newly sections

--- admin/editorsite.php
+++ admin/editorsite.php
@@ -13,31 +13,30 @@
     <div id="wrapper">
 
         <!-- Main -->
-        <section id="main">
+        <section id="main" class="main-section" data-section="1">
 
             <!-- Items Part 1-->
             <div class="items" id="part1">
                 <div class="item intro span-2">
                     <h1>D.E.N.R.</h1>
                     <p>Customer Guide Site<br /></p>
-                    <button id="addItemButton">Add Frame</button>
+                    <button class="addItemButton" data-section="1">Add Frame</button>
                 </div>
-                
             </div>  
 
             <!-- Items Part 2-->
             <div class="items" id="part2">
-            <div class="item intro span-2">
-            <button id="addSectionButton">Add Section</button>
+                <div class="item intro span-2">
+                    <button class="addSectionButton">Add Section</button>
                 </div>
             </div>
             
         </section>
-
-        <!-- Divider -->
+        
+        <!-- Footer -->
         <section id="footer">
             <section>
-                <p>           </p>
+                <p></p>
             </section>
             <section>
                 <ul class="icons">
@@ -60,51 +59,64 @@
     <!-- Custom Script -->
     <script>
     var counter = 0;
+    var sectionCounter = 1;
 
     $(document).ready(function() {
         console.log('jQuery is loaded');
 
         // Use event delegation to handle clicks on dynamically created buttons
-        $(document).on('click', '#addItemButton', function() {
-            console.log('Button clicked');
-            
+        $(document).on('click', '.addItemButton', function() {
+            console.log('Add Frame Button clicked');
+            var sectionId = $(this).data('section');
+            var targetPart = sectionId % 2 === 0 ? '#part2' : '#part1';
+
             if (counter > 3) {
                 counter = 0;
             }
 
-            var firstLane1 = $('<div class="image-container"><img src="../images/thumbs/01.jpg" alt="Thumbnail"><form action="../includes/formhandler.inc.php" method="post" class="item-form"><input type="text" name="frametitle" placeholder="Enter title" required><input type="text" name="description" placeholder="Enter description" required><input type="hidden" name="fileid" value="1"><button type="submit">Save</button></form></div> <button id="addItemButton">Add Frame</button>');
-            var firstLane2 = $('<div class="image-container"><img src="../images/thumbs/01.jpg" alt="Thumbnail"><form action="../includes/formhandler.inc.php" method="post" class="item-form"><input type="text" name="frametitle" placeholder="Enter title" required><input type="text" name="description" placeholder="Enter description" required><input type="hidden" name="fileid" value="1"><button type="submit">Save</button></form></div> <button id="addItemButton">Add Frame</button>');
-            var firstLane3 = $('<div class="image-container"><img src="../images/thumbs/01.jpg" alt="Thumbnail"><form action="../includes/formhandler.inc.php" method="post" class="item-form"><input type="text" name="frametitle" placeholder="Enter title" required><input type="text" name="description" placeholder="Enter description" required><input type="hidden" name="fileid" value="1"><button type="submit">Save</button></form></div> <button id="addItemButton">Add Frame</button>');
+            var frame = $('<div class="image-container"><img src="../images/thumbs/01.jpg" alt="Thumbnail"><form action="../includes/formhandler.inc.php" method="post" class="item-form"><input type="text" name="frametitle" placeholder="Enter title" required><input type="text" name="description" placeholder="Enter description" required><input type="hidden" name="fileid" value="1"><button type="submit">Save</button></form></div>');
 
-            if (counter == 0) {
-                $('#part1').append(firstLane1);
-            } else if (counter == 1) {
-                $('#part2').append(firstLane2);
-            } else if (counter == 2) {
-                $('#part1').append(firstLane3);
-            } else if (counter == 3) {
-                $('#part2').append(firstLane1);
-            } 
-
+            if (counter % 2 === 0) {
+                $(`#main[data-section="${sectionId}"] #part1`).append(frame);
+            } else {
+                $(`#main[data-section="${sectionId}"] #part2`).append(frame);
+            }
 
             console.log('Counter:', counter);
             counter++;
         });
 
-        $(document).on('click', '#addSectionButton', function() {
-            console.log('Button clicked');
-          
-            var firstLane1 = $('<section id="main" > <!-- Items Part 1--><div class="items" id="part1"><div class="item intro span-2"><h1>D.E.N.R.</h1><p>Customer Guide Site<br /></p><button id="addItemButton">Add Frame</button></div></div> <!-- Items Part 2--><div class="items" id="part2"><div class="item intro span-2"><button id="addSectionButton">Add Section</button></div></div> </section>');
+        $(document).on('click', '.addSectionButton', function() {
+            console.log('Add Section Button clicked');
+            sectionCounter++;
+            var newSection = $(`
+                <section id="main" class="main-section" data-section="${sectionCounter}">
+                    <!-- Items Part 1-->
+                    <div class="items" id="part1">
+                        <div class="item intro span-2">
+                            <h1>D.E.N.R.</h1>
+                            <p>Customer Guide Site<br /></p>
+                            <button class="addItemButton" data-section="${sectionCounter}">Add Frame</button>
+                        </div>
+                    </div>  
+                    <!-- Items Part 2-->
+                    <div class="items" id="part2">
+                        <div class="item intro span-2">
+                            <button class="addSectionButton">Add Section</button>
+                        </div>
+                    </div>
+                </section>
+            `);
+            
+            $('#wrapper').append(newSection);
+
             var divider = $(' <!-- Divider --><section id="footer"><section><p></p></section><section><ul class="icons"></ul><ul class="copyright"></ul></section></section>');
-            $('#wrapper').append(firstLane1);
             $('#wrapper').append(divider);
         });
-
-        
-
     });
-
-
-</script>
+    </script>
 </body>
 </html>
+
+
+
