968bcb9abd1600a2b185943abb5066112e1e4de3
Rivera <billy100007@gmail.com>
2024-06-03 01:07:30 +0800

    Saving Videos in Editor successfully Implemented

--- includes/formhandler.inc.php
+++ includes/formhandler.inc.php
@@ -1,47 +1,53 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_FILES['media']) && $_FILES['media']['error'] == UPLOAD_ERR_OK) {
+        $frametitle = $_POST["frametitle"];
+        $description = $_POST["description"];
+        $fileid = $_POST["fileid"];
 
-$frametitle = $_POST["frametitle"];
-$description = $_POST["description"];
-$fileid = $_POST["fileid"];
+        require_once "dbh.inc.php";
 
-require_once "dbh.inc.php";
+        $targetDirImages = "../storage/images/";
+        $targetDirVideos = "../storage/videos/";
+        $targetDirPPT = "../storage/ppt/";
 
-$targetDirImages = "../storage/images/";
-$targetDirVideos = "../storage/videos/";
-$targetDirPPT = "../storage/ppt/";
+        $fileName = basename($_FILES["media"]["name"]);
+        $fileTmpName = $_FILES["media"]["tmp_name"];
+        $fileType = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
 
-$fileName = basename($_FILES["media"]["name"]);
-$targetFileImage = $targetDirImages . $fileName;
-$targetFileVideo = $targetDirVideos . $fileName;
-$targetFilePPT = $targetDirPPT . $fileName;
+        // Determine the target directory based on the file type
+        if (in_array($fileType, ["jpg", "jpeg", "png", "gif"])) {
+            $targetDir = $targetDirImages;
+        } elseif (in_array($fileType, ["mp4", "avi", "mov"])) {
+            $targetDir = $targetDirVideos;
+        } elseif (in_array($fileType, ["ppt", "pptx"])) {
+            $targetDir = $targetDirPPT;
+        } else {
+            echo "Error: Unsupported file type.";
+            exit;
+        }
 
-$fileType = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+        $targetFile = $targetDir . $fileName;
 
-// Determine the target directory based on the file type
-if (in_array($fileType, ["jpg", "jpeg", "png", "gif"])) {
-    $targetFile = $targetFileImage;
-} elseif (in_array($fileType, ["mp4", "avi", "mov"])) {
-    $targetFile = $targetFileVideo;
-} elseif (in_array($fileType, ["ppt", "pptx"])) {
-    $targetFile = $targetFilePPT;
-} else {
-    echo "Error: Unsupported file type.";
-    exit;
-}
+        // Move the uploaded file to the target directory
+        if (move_uploaded_file($fileTmpName, $targetFile)) {
+            // Prepare the SQL query
+            $query = "INSERT INTO frame (frametitle, description, fileid, file_path) VALUES (?, ?, ?, ?);";
+            $stmt = $pdo->prepare($query);
 
-// Move the uploaded file to the target directory
-if (move_uploaded_file($_FILES["media"]["tmp_name"], $targetFile)) {
-    // Prepare the SQL query
-    $query = "INSERT INTO frame (frametitle, description, fileid, file_path) VALUES (?, ?, ?, ?);";
-    $stmt = $pdo->prepare($query);
-
-    // Execute the query
-    if ($stmt->execute([$frametitle, $description, $fileid, $targetFile])) {
-        echo "The file " . htmlspecialchars($fileName) . " has been uploaded and data saved.";
+            // Execute the query
+            if ($stmt->execute([$frametitle, $description, $fileid, $targetFile])) {
+                echo "The file " . htmlspecialchars($fileName) . " has been uploaded and data saved.";
+            } else {
+                echo "Error: Could not save data to database.";
+            }
+        } else {
+            echo "Error: There was an error uploading your file.";
+        }
     } else {
-        echo "Error: Could not save data to database.";
+        echo "Error: File upload error.";
     }
 } else {
-    echo "Error: There was an error uploading your file.";
+    echo "Invalid request.";
 }
 ?>
